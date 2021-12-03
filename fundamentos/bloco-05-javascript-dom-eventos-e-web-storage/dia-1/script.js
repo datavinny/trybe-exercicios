df11096e4be27a49869469c3e2c0d2005fb26af4
Davi Freitas <df828316@gmail.com>
2021-12-03 18:03:42 -0300

    5.1

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1/script.js
@@ -0,0 +1,3 @@
+document.getElementById('container').style.backgroundColor = 'blue';
+document.querySelector('.emergency-tasks').style.backgroundColor = 'orange';
+document.querySelector('.no-emergency-tasks').style.backgroundColor = 'green';
